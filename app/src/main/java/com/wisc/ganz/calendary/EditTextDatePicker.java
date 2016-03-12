b6f88b68223fb1aaef06775666374413ca023c4e
Ganz7 <ganzse7en@gmail.com>
2016-03-12 00:50:28 -0600

    Added class to create DatePickerDialog to enter date

---
+++ app/src/main/java/com/wisc/ganz/calendary/EditTextDatePicker.java
@@ -0,0 +1,22 @@
+package com.wisc.ganz.calendary;
+
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.widget.DatePicker;
+
+/**
+ * Created by Ganz on 3/12/16.
+ */
+public class EditTextDatePicker implements OnClickListener, OnDateSetListener {
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+    }
+}
