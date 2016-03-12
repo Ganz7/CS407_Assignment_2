51c8473390a5485e11662ca49bed3d9eaf01f089
Ganz7 <ganzse7en@gmail.com>
2016-03-12 17:48:49 -0600

    Building the list items

--- app/src/main/java/com/wisc/ganz/calendary/EventListCursorAdapter.java
+++ app/src/main/java/com/wisc/ganz/calendary/EventListCursorAdapter.java
@@ -2,9 +2,11 @@ package com.wisc.ganz.calendary;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
 
 /***
  * Custom Cursor Adapter for Event List
@@ -17,11 +19,15 @@ public class EventListCursorAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.event_list_item, parent, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        TextView eventTitle = (TextView) view.findViewById(R.id.event_title);
+        TextView eventTimes = (TextView) view.findViewById(R.id.event_timings);
+        TextView eventDescription = (TextView) view.findViewById(R.id.event_description);
 
+        
     }
 }
