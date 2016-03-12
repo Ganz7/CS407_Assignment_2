8deb21ef3ee026ca6d5d5223579c2ccec495a6df
Ganz7 <ganzse7en@gmail.com>
2016-03-11 23:55:05 -0600

    Added addevent code from old commit and minor changes

--- app/src/main/java/com/wisc/ganz/calendary/CreateEvent.java
+++ app/src/main/java/com/wisc/ganz/calendary/CreateEvent.java
@@ -1,10 +1,20 @@
 package com.wisc.ganz.calendary;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
+import android.provider.CalendarContract.Events;
+
+import java.util.Calendar;
 
 public class CreateEvent extends AppCompatActivity {
 
@@ -24,6 +34,58 @@ public class CreateEvent extends AppCompatActivity {
 
             }
         });
+    }
+
+    /***
+     * Adds an event with the specified arguments to the calendar
+     * @param title
+     * @param startMillis
+     * @param endMillis
+     * @param description
+     */
+    private void addEvent(String title, long startMillis, long endMillis, String description) {
+        long calID = 5;
+
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2016, 2, 14, 7, 40);
+        startMillis = beginTime.getTimeInMillis();
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2016, 2, 14, 8, 40);
+        endMillis = endTime.getTimeInMillis();
+
+        ContentResolver cr = getContentResolver();
+        ContentValues values = new ContentValues();
+        values.put(Events.DTSTART, startMillis);
+        values.put(Events.DTEND, endMillis);
+        values.put(Events.TITLE, title);
+        values.put(Events.DESCRIPTION, description);
+        values.put(Events.CALENDAR_ID, calID);
+        values.put(Events.EVENT_TIMEZONE, "America/Chicago");
+
+        checkForAndRequestPermission();
+
+        Uri uri = cr.insert(Events.CONTENT_URI, values);
+
+        long eventID = Long.parseLong(uri.getLastPathSegment());
+        Toast.makeText(this, "Event ID is" + eventID, Toast.LENGTH_LONG).show();
+    }
+
+    private void extractDataAndAddEvent(){
+
+    }
+
+    /***
+     * Checks if Calendar Read/Write permission has been granted. If not,
+     * Requests for it.
+     */
+    private void checkForAndRequestPermission(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission_group.CALENDAR) !=
+                PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission_group.CALENDAR},
+                    CalendarMainActivity.MY_PERMISSIONS_REQUEST_RW_CALENDAR);
+            return;
+        }
     }
 
 }
